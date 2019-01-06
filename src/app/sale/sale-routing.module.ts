2006de05c1a4a21684f3c16823055cc47bbe25dd
dattudigital <dattu@digitalrupay.com>
2019-01-06 17:12:27 +0530

    sales-module

--- src/app/sale/sale-routing.module.ts
+++ src/app/sale/sale-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from '../../common-session/session.check'
 import { DashboardComponent } from './sale-dashboard/dashboard.component';
-import { NewTicketComponent } from './new-ticket/new-ticket.component'
+import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { NewTicketNextButtonComponent } from './new-ticket-next-button/new-ticket-next-button.component';
+import { NewTicketNextButtonInvoiceComponent } from './new-ticket-next-button-invoice/new-ticket-next-button-invoice.component';
+import { NewTicketInvoicePaymentComponent } from './new-ticket-invoice-payment/new-ticket-invoice-payment.component'
 
 const routes: Routes = [
   {
@@ -24,6 +27,21 @@ const routes: Routes = [
         path: 'new-ticket',
         component: NewTicketComponent,
         canActivate: [AuthGuard]
+      },
+      {
+        path: 'new-ticket-next-button',
+        component: NewTicketNextButtonComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path:'new-ticket-next-button-invoice',
+        component: NewTicketNextButtonInvoiceComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path:'new-ticket-next-button-invoice-payment',
+        component:NewTicketInvoicePaymentComponent,
+        canActivate:[AuthGuard]
       }
     ]
   }
--- src/app/sale/sale.module.ts
+++ src/app/sale/sale.module.ts
@@ -2,20 +2,32 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { saleRoutingModule } from './sale-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { AuthGuard } from '../../common-session/session.check';
 import { DashboardComponent } from './sale-dashboard/dashboard.component';
-import { NewTicketComponent } from './new-ticket/new-ticket.component'
+import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { NewTicketNextButtonComponent } from './new-ticket-next-button/new-ticket-next-button.component';
+import { NewTicketNextButtonInvoiceComponent } from './new-ticket-next-button-invoice/new-ticket-next-button-invoice.component';
+import { NewTicketInvoicePaymentComponent } from './new-ticket-invoice-payment/new-ticket-invoice-payment.component'
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 @NgModule({
   imports: [
     CommonModule,
     saleRoutingModule,
+    Ng2SearchPipeModule,
     FormsModule,
-    ReactiveFormsModule
+    TypeaheadModule.forRoot(),
+    ReactiveFormsModule,
+    NgxPaginationModule
   ],
   declarations: [
     DashboardComponent,
-    NewTicketComponent
+    NewTicketComponent,
+    NewTicketNextButtonComponent,
+    NewTicketNextButtonInvoiceComponent,
+    NewTicketInvoicePaymentComponent
   ],
   providers: [AuthGuard]
 })
