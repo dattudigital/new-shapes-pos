1d784d2a0a10647159781ee0d9e7beb09c206a83
dattudigital <dattu@digitalrupay.com>
2019-02-27 17:17:53 +0530

    loading secondary bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError, Event} from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'car';
+  
+  showLoadingIndicator = true;
+
+  // Inject the Angular Router
+  constructor(private _router: Router) {
+    // Subscribe to the router events observable
+    this._router.events.subscribe((routerEvent: Event) => {
+
+      // On NavigationStart, set showLoadingIndicator to ture
+      if (routerEvent instanceof NavigationStart) {
+        this.showLoadingIndicator = true;
+      }
+
+      // On NavigationEnd or NavigationError or NavigationCancel
+      // set showLoadingIndicator to false
+      if (routerEvent instanceof NavigationEnd ||
+        routerEvent instanceof NavigationError ||
+        routerEvent instanceof NavigationCancel) {
+        this.showLoadingIndicator = false;
+      }
+
+    });
+  }
 }
